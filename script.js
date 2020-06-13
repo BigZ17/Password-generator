72f4b483f8316429d574561d3ad6572e922fc3bd
wacky@Zacs-Desktop <zcraven17@gmail.com>
2020-06-13 01:41:45 -0700

    added password options

--- script.js
+++ script.js
@@ -101,6 +101,43 @@ var numericcharacters
 ];
 
 // Get references to the #generate element
+function getpasswordoptions() {
+    var length = parseInt(
+        prompt("How many characters would you like for your password")
+    );
+    
+    if (isNaN(length) === true) {
+        alert('Password lenght must be provided as a number');
+        return;
+    }
+    
+    if (length < 8) {
+        alert('Password must be more than 8 characters');
+        return;
+    }
+    
+    if (length > 128) {
+        alert('Password must be less than 128 characters');
+        return;
+    }
+    
+    var hasspecialcharacters = confirm('Click yes to confirm to include special chararcters');
+
+    var hasNumericcharacter = confirm('Click yes to confirm to include numbers');
+    
+    var haslowercaseletters = confirm('Click yes to confirm to include lowercase letters');
+    
+    var hasUPPERCASELETTERS = confirm('Click yes to confirm to include uppercase letters');
+    if (
+        hasspecialcharacters === false &&
+        hasNumericcharacter === false &&
+        haslowercaseletters === false &&
+        hasUPPERCASELETTERS === false
+    ){
+        alert('Must select atleast one character set')
+        return;
+    }
+};
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
